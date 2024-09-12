0a25dc03cd71d6e4c8d984a5b430f2156f749d41
edwinochieng <ochiengedwin440@gmail.com>
2024-09-12 23:20:11 +0300

    add longest-palindrome solution

---
+++ Strings/longest_palindrome.py
@@ -0,0 +1,15 @@
+def longestPalindrome(s):
+    seen = set()
+    result = 0
+
+    for char in s:
+        if char in seen:
+            seen.remove(char)
+            result +=2
+        else:
+            seen.add(char)
+    
+    if seen:
+        result +=1
+    
+    return result
