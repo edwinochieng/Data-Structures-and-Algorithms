ff167485b8a5ef49d156e333a18a8fa6823e576d
edwinochieng <ochiengedwin440@gmail.com>
2024-09-23 20:19:50 +0300

    add solution to 3 Sum problem

---
+++ Arrays/3sum.py
@@ -0,0 +1,25 @@
+def threeSum(nums):
+    res = []
+    nums.sort()
+
+    for i,a in enumerate(nums):
+        if i > 0 and a == nums[i -1 ]:
+            continue
+
+        l = i + 1
+        r = len(nums)-1
+
+        while l < r:
+            currSum = a + nums[l] + nums[r]
+
+            if currSum > 0:
+                r -= 1
+            elif currSum < 0:
+                l += 1
+            else:
+                res.append([a,nums[l],nums[r]])
+                l += 1
+                while l < r and nums[l] == nums[l - 1]:
+                    l += 1
+
+    return res
