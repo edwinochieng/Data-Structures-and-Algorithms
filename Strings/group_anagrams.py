2e04680074e204f109a70492728cfff310405e77
edwinochieng <ochiengedwin440@gmail.com>
2024-09-26 23:52:48 +0300

    add solution to group anagrams

---
+++ Strings/group_anagrams.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    res = defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26
+
+        for c in s:
+            count[ord(c) - ord("a")] +=1
+
+        res[tuple(count)].append(s)
+
+    return res.values()
